7e1c833bf75d5b144ac4458c0a94e177ca454241
Jake Simpson <102685368+Jacob-at-BOL@users.noreply.github.com>
2024-07-22 15:28:10 -0400

    Initial Commit

---
+++ Basic Social/Basic_SocialApp.swift
@@ -0,0 +1,17 @@
+//
+//  Basic_SocialApp.swift
+//  Basic Social
+//
+//  Created by Jacob on 7/22/24.
+//
+
+import SwiftUI
+
+@main
+struct Basic_SocialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Basic Social/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Basic Social
+//
+//  Created by Jacob on 7/22/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
